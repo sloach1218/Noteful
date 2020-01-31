9bb36727ae6f590367ec750229936b39d381110f
Sarah Loach <sloach1218@gmail.com>
2020-01-30 21:44:08 -0600

    added create note functionality and validation

--- src/AddFolderMain.js
+++ src/AddFolderMain.js
@@ -1,7 +1,8 @@
 import React from 'react';
 //import { NavLink } from 'react-router-dom';
-import NotesContext from './NotesContext'
+import NotesContext from './NotesContext';
 //import { updateNotesBasedOnFolder } from './noteHelpers'
+import ValidationError from './ValidationError'
 
 //import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -38,16 +39,14 @@ class AddFolderMain extends React.Component {
       window.location.href = '/';
     })
     
-
-    
   }
 
   validateName() {
     const name = this.state.name.value.trim();
     if (name.length === 0) {
       return "Name is required";
-    } else if (name.length < 3) {
-      return "Name must be at least 3 characters long";
+    } else if (name.length < 2) {
+      return "Name must be at least 2 characters long";
     }
   }
  
@@ -60,7 +59,8 @@ class AddFolderMain extends React.Component {
         <legend>Create a folder</legend>
         <label htmlFor="name">Folder Name:</label>
         <input type="text" name="name" id="name" onChange={e => this.updateName(e.target.value)}></input>
-        <button type="submit">Submit</button>
+        {this.state.name.touched && (<ValidationError message={this.validateName()} />)}
+        <button type="submit" disabled={ this.validateName() }>Submit</button>
       </form>
     );
   }
--- src/AddNoteMain.js
+++ src/AddNoteMain.js
@@ -1,30 +1,99 @@
 import React from 'react';
 import NotesContext from './NotesContext'
-import { updateNotesBasedOnFolder } from './noteHelpers'
+//import { updateNotesBasedOnFolder } from './noteHelpers'
+import ValidationError from './ValidationError';
 
 
 
 class AddNoteMain extends React.Component {
 
   static contextType = NotesContext;
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: {
+        value: "",
+        touched: false
+      },
+      content: {
+        value: "",
+        touched: false
+      },
+      noteFolder: {
+        value: "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
+      },
+    };
+  }
+  updateName(name) {
+    this.setState({ name: { value: name, touched: true } });
+  }
+  updateContent(content) {
+    this.setState({ content: { value: content, touched: true } });
+  }
+  updateFolder(folder) {
+    this.setState({ noteFolder: { value: folder, touched: true } });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    const { name, content, noteFolder } = this.state;
+    const date = new Date(); //Thu Jan 30 2020 18:45:11 GMT-0600 (Central Standard Time)
+    const data = {
+      "name": name.value,
+      "content": content.value,
+      "modified": "2019-01-03T00:00:00.000Z",
+      "folderId": noteFolder.value,
+    }
+
+    fetch('http://localhost:9090/notes', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    })
+    .then(() => {
+      window.location.href = '/';
+    })
+    
+  }
+  validateName() {
+    const name = this.state.name.value.trim();
+    if (name.length === 0) {
+      return "Name is required";
+    } else if (name.length < 2) {
+      return "Name must be at least 2 characters long";
+    }
+  }
+
+  validateContent() {
+    const name = this.state.content.value.trim();
+    if (name.length === 0) {
+      return "Name is required";
+    } else if (name.length < 5) {
+      return "Name must be at least 5 characters long";
+    }
+  }
   
  
 
   render(){
-    
+    const folders = this.context.folders;
     
     return (
-      <form>
+      <form onSubmit={e => this.handleSubmit(e)}>
         <legend>Create a note</legend>
         <label htmlFor="name">Note Name:</label>
-        <input type="text" name="name" id="name"></input>
+        <input type="text" name="name" id="name" onChange={e => this.updateName(e.target.value)}></input>
+        {this.state.name.touched && (<ValidationError message={this.validateName()} />)}
         <label htmlFor="content">Content:</label>
-        <textarea type="text" name="content" id="content"></textarea>
-        <select name="cars">
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
-          <option value="fiat">Fiat</option>
-          <option value="audi">Audi</option>
+        <textarea type="text" name="content" id="content" onChange={e => this.updateContent(e.target.value)}></textarea>
+        {this.state.content.touched && (<ValidationError message={this.validateContent()} />)}
+        <label htmlFor="folders">Select a Folder:</label>
+        <select name="folders" value={this.state.noteFolder.value} onChange={e => this.updateFolder(e.target.value)}>
+          {folders.map((folder) => (
+          <option value={folder.id} key={folder.id}>{folder.name}</option>
+          ))}
+          
         </select>
         <button type="submit">Submit</button>
       </form>

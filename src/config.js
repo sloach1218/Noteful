bb5eae146ab30f8bbdd22d1997df209a2ac86d16
Sarah Loach <Sarah@Sarahs-fat-top.local>
2020-03-27 12:57:46 -0500

    fix mixed content error

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_NOTES_ENDPOINT: `//guarded-wildwood-03458.herokuapp.com/api/notes`,
-    API_FOLDERS_ENDPOINT: `//guarded-wildwood-03458.herokuapp.com/api/folders`,
+    API_NOTES_ENDPOINT: `https://guarded-wildwood-03458.herokuapp.com/api/notes`,
+    API_FOLDERS_ENDPOINT: `https://guarded-wildwood-03458.herokuapp.com/api/folders`,
 }
